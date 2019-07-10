01f3d30588fc8a7cd7a733a8a7eed1fda1c6ac35
Karthik Puttur Sooryanarayana <karthikps27@gmail.com>
2019-07-10 22:08:26 +1000

    Restructuring

---
+++ src/main/java/org/karthikps/User.java
@@ -0,0 +1,70 @@
+package org.karthikps;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+
+public class User implements Serializable{
+
+	private String firstname;
+	private String lastname;
+	private Date dob;
+	private long phonenumber;
+	private String email;
+	
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(
+		name = "UUID",
+		strategy = "org.hibernate.id.UUIDGenerator"
+	)
+	@Column(name = "userid", updatable = false, nullable = false)
+	private String userid;
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public long getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(long phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid() {
+		this.userid = UUID.randomUUID().toString();
+	}
+	
+}
