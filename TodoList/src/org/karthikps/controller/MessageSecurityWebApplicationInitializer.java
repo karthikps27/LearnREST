84fb4362cfb9e2aca3845adccff75838c17c6b8c
Karthik Puttur Sooryanarayana <karthikps27@gmail.com>
2019-07-10 22:10:41 +1000

    Deleting

--- TodoList/src/org/karthikps/controller/MessageSecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package org.karthikps.controller;
-
-import org.springframework.security.web.context.*;
-
-public class MessageSecurityWebApplicationInitializer
-      extends AbstractSecurityWebApplicationInitializer {
-}
--- testTern/test.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-describe("testFunction", function() {
-	it("testMethod", function() {
-		browser.get("www.google.co.in");
-		browser.sleep(5000);
-	})
-})
